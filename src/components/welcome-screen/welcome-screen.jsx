7c972b4a4537068a5de0eae18af21f88dabd222e
Eduard <eduard66_06@list.ru>
2019-05-15 16:36:55 +0500

    fix prop valid

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const WelcomeScreen = (props) => {
   const {time, errorCount} = props;
@@ -18,5 +19,9 @@ const WelcomeScreen = (props) => {
   </section>;
 };
 
+WelcomeScreen.propTypes = {
+  time: PropTypes.number,
+  errorCount: PropTypes.number
+};
 
 export default WelcomeScreen;
